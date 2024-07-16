0863593f6b1a31abaa4fa80b894c88b185eac6a4
kedabudre <abdurehimk77@gmail.com>
2024-07-16 18:53:21 +0300

    update file

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -5,9 +5,9 @@ import Contacts from "./scenes/contacts";
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
+import Invoices from "./scenes/invoices";
 import Team from "./scenes/team";
 import { ColorModeContext, useMode } from "./theme";
-// import Invoices from "./scenes/invoices";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -29,7 +29,7 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
