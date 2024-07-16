bf01526d54e5c3ac0632d6191d1b0dda4a3e57b6
kedabudre <abdurehimk77@gmail.com>
2024-07-17 00:01:14 +0300

    create a form

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -3,13 +3,13 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Contacts from "./scenes/contacts";
 import Dashboard from "./scenes/dashboard";
+import Form from "./scenes/form";
 import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
 import Invoices from "./scenes/invoices";
 import Team from "./scenes/team";
 import { ColorModeContext, useMode } from "./theme";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Calendar from "./scenes/calendar/calendar";
 
@@ -30,12 +30,12 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
