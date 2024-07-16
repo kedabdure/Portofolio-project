9b30c55b56555a0d5c6708602fdb69c313d5d950
kedabudre <abdurehimk77@gmail.com>
2024-07-16 15:01:38 +0300

    create header component

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,6 +1,9 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
 import { Routes } from "react-router-dom";
-// import Sidebar from "./scenes/global/Sidebar";
+import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
 // import Dashboard from "./scenes/dashboard";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
@@ -8,29 +11,24 @@ import Topbar from "./scenes/global/Topbar";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
 // import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
-//   const [isSidebar, setIsSidebar] = useState(true);
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          {/* <Sidebar isSidebar={isSidebar} /> */}
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Topbar  />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               {/* <Route path="/" element={<Dashboard />} /> */}
               {/* <Route path="/team" element={<Team />} /> */}
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/conta cts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
