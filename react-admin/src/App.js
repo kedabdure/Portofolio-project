786dd2ba55a92a2c92bfad61676c27ea46830cfa
kedabudre <abdurehimk77@gmail.com>
2024-07-17 04:24:16 +0300

    implemente calandar

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -11,7 +11,7 @@ import Team from "./scenes/team";
 import { ColorModeContext, useMode } from "./theme";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
-// import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -31,7 +31,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
