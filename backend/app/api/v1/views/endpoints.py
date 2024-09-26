2f79bd02c5744a9ec19c5b323995affd21eff2b5
kedabudre <abdurehimk77@gmail.com>
2024-09-26 14:32:22 +0200

    update file

--- backend/app/api/v1/views/endpoints.py
+++ backend/app/api/v1/views/endpoints.py
@@ -5,8 +5,30 @@ from .. import api_v1
 @api_v1.route('/', methods=['GET'])
 @api_v1.route('/hello', methods=['GET'])
 def hello():
+    """
+    This function returns a JSON response with a welcome message.
+
+    The function is designed to handle GET requests at the root ('/') and '/hello' endpoints.
+    It returns a JSON response with a 'message' key containing a welcome message.
+
+    Parameters:
+    None
+
+    Returns:
+    dict: A JSON response with a 'message' key containing the welcome message.
+    """
     return jsonify({"message": "Hello, Welcome Ex.Handyman Services!"})
+
 
 @api_v1.route('/goodbye', methods=['GET'])
 def goodbye():
+    """
+    This function returns a JSON response with a goodbye message.
+
+    Parameters:
+    None
+
+    Returns:
+    dict: A JSON response with a 'message' key containing the goodbye message.
+    """
     return jsonify({"message": "Goodbye, World!"})
